32901037a85e7c1522291b0283e4b00aa102e7b1
yuvkess <yuval.kessel@post.idc.ac.il>
2018-11-28 20:47:46 +0200

    removed job intent service

--- EXERCISE_03/app/src/main/java/milab/idc/com/exercise_03/MainActivity.java
+++ EXERCISE_03/app/src/main/java/milab/idc/com/exercise_03/MainActivity.java
@@ -1,9 +1,5 @@
 package milab.idc.com.exercise_03;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.Service;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,13 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        QuoteJobService.startActionInit(this);
-
         QuoteNotificationService.startActionInit(this);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-//            QuoteJobService.startActionInit(this);
-//        } else {
-//            QuoteNotificationService.startActionInit(this);
-//        }
     }
 }
