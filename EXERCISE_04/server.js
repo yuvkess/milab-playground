9129308aae0aea7f176b89d582828ea465d69d0f
yuvkess <yuval.kessel@post.idc.ac.il>
2018-12-02 22:49:01 +0200

    added little fix for undefined

--- EXERCISE_04/server.js
+++ EXERCISE_04/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 let server = http.createServer(function(request, response) {
+  let url = (request.url == undefined) ? "" : request.url;
   response.writeHead(200);
-  response.end(request.headers.host+request.url);
+  response.end(request.headers.host + url);
 });
 
 server.listen(8080);
